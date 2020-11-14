4c0fe85f9c10b7adbae9cd96cd74f92f45766832
321 123 <none>
2020-11-14 08:30:20 +0700

    v1.8 - run AlertDialog.Builder.Create on UI thread

--- Okkei Patcher/MessageBox.cs
+++ Okkei Patcher/MessageBox.cs
@@ -58,7 +58,7 @@ namespace OkkeiPatcher
 					break;
 			}
 
-			builder.Create()?.Show();
+			MainThread.BeginInvokeOnMainThread(() => { builder.Create()?.Show(); });
 		}
 
 		private static void MessageBoxOkUnknownAppSourceNoticeAction(object sender, DialogClickEventArgs e)
