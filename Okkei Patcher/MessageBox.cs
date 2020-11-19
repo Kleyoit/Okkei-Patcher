f1bbb2e824c1cbb993ccefca97a10be8c4706d9a
321 123 <none>
2020-11-19 13:09:08 +0700

    Unwanted commit

--- Okkei Patcher/MessageBox.cs
+++ Okkei Patcher/MessageBox.cs
@@ -1,29 +1,14 @@
-﻿using Android.App;
+﻿using System;
+using Android.App;
 using Android.Content;
+using Android.Provider;
 using Xamarin.Essentials;
+using Uri = Android.Net.Uri;
 
 namespace OkkeiPatcher
 {
 	public static class MessageBox
 	{
-		private static Activity _getMessageBoxActivity;
-
-		public struct Data
-		{
-			public string Title { get; }
-			public string Message { get; }
-			public Code Id { get; }
-
-			public static Data Empty => new Data(string.Empty, string.Empty, Code.OK);
-
-			public Data(string title, string message, Code id)
-			{
-				Title = title;
-				Message = message;
-				Id = id;
-			}
-		}
-
 		public enum Code
 		{
 			OK,
@@ -31,8 +16,12 @@ namespace OkkeiPatcher
 			Exit
 		}
 
-		public static void Show(Activity callerActivity, string title, string message, Code id) =>
+		private static Activity _getMessageBoxActivity;
+
+		public static void Show(Activity callerActivity, string title, string message, Code id)
+		{
 			Show(callerActivity, new Data(title, message, id));
+		}
 
 		public static void Show(Activity callerActivity, Data data)
 		{
@@ -63,12 +52,30 @@ namespace OkkeiPatcher
 
 		private static void MessageBoxOkUnknownAppSourceNoticeAction(object sender, DialogClickEventArgs e)
 		{
-			Intent intent = new Intent(Android.Provider.Settings.ActionManageUnknownAppSources,
-				Android.Net.Uri.Parse("package:" + AppInfo.PackageName));
+			var intent = new Intent(Settings.ActionManageUnknownAppSources,
+				Uri.Parse("package:" + AppInfo.PackageName));
 			_getMessageBoxActivity.StartActivityForResult(intent, (int) GlobalData.RequestCodes.UnknownAppSourceCode);
 		}
 
-		private static void MessageBoxExitAction(object sender, DialogClickEventArgs e) =>
-			System.Environment.Exit(0);
+		private static void MessageBoxExitAction(object sender, DialogClickEventArgs e)
+		{
+			Environment.Exit(0);
+		}
+
+		public struct Data
+		{
+			public string Title { get; }
+			public string Message { get; }
+			public Code Id { get; }
+
+			public static Data Empty => new Data(string.Empty, string.Empty, Code.OK);
+
+			public Data(string title, string message, Code id)
+			{
+				Title = title;
+				Message = message;
+				Id = id;
+			}
+		}
 	}
 }
