f148ac2372ce7a55b54aa36f8cb22733d6744906
321 123 <none>
2020-11-19 23:56:33 +0700

    Patcher assembly info

--- Patcher/Properties/AssemblyInfo.cs
+++ Patcher/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Patcher")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("©2020 solru. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
