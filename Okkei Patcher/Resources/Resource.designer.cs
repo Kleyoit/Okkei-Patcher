5271c438923c1759e305211a2d2069a62a748fb2
321 123 <none>
2021-01-07 23:12:21 +0700

    Resources auto-update

--- Okkei Patcher/Resources/Resource.designer.cs
+++ Okkei Patcher/Resources/Resource.designer.cs
@@ -25,6 +25,7 @@ namespace OkkeiPatcher
 		
 		public static void UpdateIdValues()
 		{
+			global::SignApk.Resource.String.app_name = global::OkkeiPatcher.Resource.String.app_name;
 			global::Xamarin.Essentials.Resource.Attribute.alpha = global::OkkeiPatcher.Resource.Attribute.alpha;
 			global::Xamarin.Essentials.Resource.Attribute.font = global::OkkeiPatcher.Resource.Attribute.font;
 			global::Xamarin.Essentials.Resource.Attribute.fontProviderAuthority = global::OkkeiPatcher.Resource.Attribute.fontProviderAuthority;
